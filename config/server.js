babbef991a8c2df8490a9cb4b887021d604a71fd
Guillermo Rodas <glrodasz@gmail.com>
2020-01-10 22:44:30 +0100

    Update environment variable configuration

--- config/server.js
+++ config/server.js
@@ -4,9 +4,9 @@ const serverConfig = {
   isProduction: process.env.NODE_ENV === 'production',
   mailchimpApiKey: process.env.MAILCHIMP_API_KEY,
   mailchimpAudienceId: process.env.MAILCHIMP_AUDIENCE_ID,
-  wordpressUrl: process.env.WORDPRESS_URL,
   wpConsumerKey: process.env.WP_CONSUMER_KEY,
   wpConsumerSecret: process.env.WP_CONSUMER_SECRET,
+  wordpressUrl: process.env.WORDPRESS_URL,
   ePaycoClientId: process.env.EPAYCO_CLIENT_ID,
   ePaycoSecretKey: process.env.EPAYCO_SECRET_KEY
 }
